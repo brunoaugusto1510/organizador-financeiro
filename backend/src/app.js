aab789f505bed4dd8a5ee13a6217962e4cd70565
Bruno Augusto <brunoaugustomaciel@gmail.com>
2026-06-01 12:26:34 -0300

    feat - estrutura inicial do backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,16 @@
+import express from 'express';
+import cors from 'cors';
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: 'API do Organizador Financeiro funcionando.',
+  });
+});
+
+export default app;
